c2dff21b573d19317477551881dd8e71b61d585e
Tee Parham <tee@stackpilot.com>
2012-04-21 07:29:01 -0700

    fix git bash completion path

--- bash/path.sh
+++ bash/path.sh
@@ -8,4 +8,4 @@ export PATH=/usr/local/mysql/bin:$PATH
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 # git bash completion
-source `brew --prefix git`/etc/bash_completion.d/git-completion.bash
+source /usr/local/etc/bash_completion.d/git-completion.bash
