340b0424672b419713e1591f3935e51d48a08039
Tee Parham <tee@neighborland.com>
2012-12-12 15:05:30 -0700

    add bash history env settings

--- bash/env.sh
+++ bash/env.sh
@@ -1,5 +1,7 @@
 export DISPLAY=0.0
 export HISTCONTROL=ignoredups
+export HISTSIZE=99999
+export HISTFILESIZE=99999
 export RUBYLIB="/usr/local/lib:$RUBYLIB"
 export EDITOR=vim
 export BREW=/usr/local/Library/Formula
