5c2504014095a011efae9286720a344c5eb42f94
Tee Parham <tee@neighborland.com>
2013-01-07 12:43:35 -0700

    set env for ruby falcon patch

--- bash/env.sh
+++ bash/env.sh
@@ -3,6 +3,8 @@ export HISTCONTROL=ignoredups
 export HISTSIZE=99999
 export HISTFILESIZE=99999
 export RUBYLIB="/usr/local/lib:$RUBYLIB"
+export RUBY_GC_MALLOC_LIMIT=60000000
+export RUBY_FREE_MIN=200000
 export EDITOR=vim
 export BREW=/usr/local/Library/Formula
 
