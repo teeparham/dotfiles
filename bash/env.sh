8fe648c6ba7cb4e2258b71de08ac60d994e20909
Tee Parham <tee@neighborland.com>
2013-09-06 09:34:00 -0600

    remove RUBYLIB

--- bash/env.sh
+++ bash/env.sh
@@ -2,7 +2,6 @@ export DISPLAY=0.0
 export HISTCONTROL=ignoredups
 export HISTSIZE=99999
 export HISTFILESIZE=99999
-export RUBYLIB="/usr/local/lib:$RUBYLIB"
 export RUBY_GC_MALLOC_LIMIT=60000000
 export RUBY_FREE_MIN=200000
 export EDITOR=vim
