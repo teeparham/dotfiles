a947614fa1c356734e423d669460e0823c458f25
Tee Parham <tee@neighborland.com>
2014-08-12 10:21:50 -0600

    Set atom as bundler editor

--- bash/env.sh
+++ bash/env.sh
@@ -3,6 +3,7 @@ export HISTCONTROL=ignoredups
 export HISTSIZE=99999
 export HISTFILESIZE=99999
 export RUBY_GC_MALLOC_LIMIT=60000000
+export BUNDLER_EDITOR=atom
 export EDITOR=vim
 export BREW=/usr/local/Library/Formula
 export PS1="\[$(tput setaf 2)\]\W $ \[$(tput sgr0)\]"
