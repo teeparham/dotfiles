bdd09369d737f93a3cec458d7b9c993de2555a4c
Tee Parham <tee@neighborland.com>
2017-04-06 13:57:43 -0600

    rm ruby malloc limit

--- bash/env.sh
+++ bash/env.sh
@@ -2,7 +2,6 @@ export DISPLAY=0.0
 export HISTCONTROL=ignoredups
 export HISTSIZE=99999
 export HISTFILESIZE=99999
-export RUBY_GC_MALLOC_LIMIT=60000000
 export BUNDLER_EDITOR=atom
 export EDITOR=vim
 export BREW=/usr/local/Library/Formula
