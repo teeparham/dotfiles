6225844793068d1bd89e4ac81724c2d092aaaa57
Tee Parham <tee@neighborland.com>
2018-08-14 10:36:44 -0600

    Add ~/.ruby to $PATH

--- bash/path.sh
+++ bash/path.sh
@@ -1,5 +1,5 @@
 export GOPATH=/Users/tee/gocode
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$GOPATH/bin:$PATH
+export PATH=/Users/tee/.ruby:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$GOPATH/bin:$PATH
 
 # git bash completion
 source /usr/local/etc/bash_completion.d/git-completion.bash
