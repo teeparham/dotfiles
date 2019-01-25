910e95d2fc1f723380b7cdf2c74c6c68e0d951f5
Tee Parham <tee@neighborland.com>
2019-01-25 09:35:58 -0700

    Add ~/.cargo/bin to path

--- bash/path.sh
+++ bash/path.sh
@@ -1,5 +1,5 @@
 export GOPATH=/Users/tee/gocode
-export PATH=/Users/tee/.ruby:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$GOPATH/bin:$PATH
+export PATH=/Users/tee/.ruby:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$GOPATH/bin:$HOME/.cargo/bin:$PATH
 
 # git bash completion
 source /usr/local/etc/bash_completion.d/git-completion.bash
