59d0ed0128352929644f70022f895b673bee137d
Tee Parham <tee@neighborland.com>
2019-03-12 18:11:04 -0600

    Add rr() - run N times

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,6 +1,13 @@
 function title {
   printf "\033]0;%s\007" "$1";
 }
+
 function ptt {
   PARALLEL_TEST_PROCESSORS=4 rake parallel:test[^test/$1];
+}
+
+function rr() {
+  for ((n=0; n<$1; n++))
+    do eval ${*:2}
+  done
 }
