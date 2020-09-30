3bac9f4a23ec131b966420a5c8a9686c9ca4348a
Tee Parham <parhameter@gmail.com>
2020-09-30 13:12:42 -0600

    Update prompt - full path

--- bash/env.sh
+++ bash/env.sh
@@ -5,7 +5,7 @@ export HISTFILESIZE=99999
 export BUNDLER_EDITOR=atom
 export EDITOR=vim
 export BREW=/usr/local/Library/Formula
-export PS1="\[$(tput setaf 2)\]\W $ \[$(tput sgr0)\]"
+export PS1="\[$(tput setaf 2)\]\w $ \[$(tput sgr0)\]"
 export PARALLEL_TEST_PROCESSORS=6
 
 ssh-add
