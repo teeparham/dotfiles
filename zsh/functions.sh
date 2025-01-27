480723e61a5155531da16279d83fd7f1abc5b8fd
Tee Parham <parhameter@gmail.com>
2025-01-27 16:48:24 -0700

    Update functions, add utr

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,19 +1,32 @@
-function title {
+title() {
   printf "\033]0;%s\007" "$1";
 }
 
-function ptt {
+ptt() {
   PARALLEL_TEST_PROCESSORS=4 rake parallel:test[^test/$1];
 }
 
-function rr() {
+rr() {
   for ((n=0; n<$1; n++))
     do eval ${*:2}
   done
 }
 
 # brew install ffmpeg imagemagick
-function mov2gif() {
+mov2gif() {
   ffmpeg -i "$1" -vf scale=800:-1 -r 10 -f image2pipe -vcodec ppm - |\
   convert -delay 5 -layers Optimize -loop 0 - "${1%.*}.gif"
+}
+
+utrs() {
+  local url="https://api.utrsports.net/v2/search?query=$1&top=7&skip=0"
+  local raw=$(curl -s "$url")
+  local response=$(echo "$raw" | tr -d '\000-\037')
+  echo "$response" | jq -r '.players.hits[].source | "\(.displayName) - \(.location.display) - \(.id)"'
+}
+
+utr() {
+  local url="https://api.utrsports.net/v1/player/$1/profile"
+  local response=$(curl -s $url)
+  echo "$response" | jq '.threeMonthRating'
 }
